eb2d82cd13c5bea683abab310c8ccd3ff72d72ae
merel281 <merelreus@gmail.com>
2026-04-25 12:20:09 +0200

    Update srun-merel.sh

--- Clusterscripts/srun-merel.sh
+++ Clusterscripts/srun-merel.sh
@@ -1,5 +1,5 @@
-# Runs CLusterJob.sh <Input.fasta> 
+# Runs CLusterJob.sh <Input.fasta> <BioLiP_*.txt>
 # Runs in ADA defq
 
-srun -N 1 -p defq ./ClusterJob.sh metal.fasta 
+srun -N 1 -p defq ./ClusterJob.sh peptide.fasta BioLiP_peptide.txt
 #srun -N 1 -p binf  ./merel.sh
